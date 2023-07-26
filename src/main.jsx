585c20dc8acad3b566589e4d09786da2a36d06ee
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-26 15:30:13 -0300

    feat: Add Header and Footer  components to main layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from "./styles/global"
-import theme from "./styles/theme"
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+import theme from "./styles/theme";
 
 import Home from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+import Header from "./components/Header";
+import HeaderDesktop from "./components/HeaderDesktop";
+
+import { useMediaQuery } from "react-responsive";
+import Footer from "./components/Footer";
+
+
+export default function App() {
+  const isMobile = useMediaQuery({ maxWidth: 1024 });
+
+  return (
+    <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
+      <GlobalStyles />
+        {isMobile ? <Header /> : <HeaderDesktop />}
       <Home />
+      <Footer />
     </ThemeProvider>
   </React.StrictMode>
-);
+  )
+}
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
