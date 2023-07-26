d13cb39ca0d2071524251764dc1c7a7d6b876637
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-26 15:31:01 -0300

    feat: Add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function useLocalStorage(item) {
+  const [value, setValue] = useState(JSON.parse(localStorage.getItem(item)) ?? '');
+
+  const updateLocalStorage = (newValue) => {
+    setValue(newValue)
+    localStorage.setItem(item, JSON.stringify(newValue));
+  }
+
+  return {
+    value,
+    updateLocalStorage
+  }
+};
