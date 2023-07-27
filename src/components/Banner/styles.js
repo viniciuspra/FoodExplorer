727f94b6a36dfa6910f51efad27af5d72e8cc92c
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-27 10:34:21 -0300

    style: Update banner size and alignment

--- src/components/Banner/styles.js
+++ src/components/Banner/styles.js
@@ -2,16 +2,26 @@ import styled from "styled-components";
 import { media } from "../../../breakpoints";
 
 export const Container = styled.div`
-  padding: 4rem 2.4rem;
-  ${media.greaterThan("tablet")`
-    padding: 12rem 10rem;
+  padding: 16rem 2.4rem 4rem;
+
+  ${media.greaterThan("mobile")`
+    padding: 18rem 9.5rem 10rem;
   `}
+  
+  ${media.greaterThan("tablet")`
+    padding: 21rem 9.5rem 10rem;
+  `}
+
+  ${media.greaterThan("desktop")`
+    padding: 26rem 10rem 12rem;
+  `}
+
 `;
 
 export const ContentWrapper = styled.div`
   height: 12rem;
   display: flex;
-  gap: 16rem;
+  gap: 15rem;
   padding: 1.6rem 0.5rem;
   border-radius: 0.5rem;
 
@@ -22,7 +32,9 @@ export const ContentWrapper = styled.div`
   `}
 
   ${media.greaterThan("desktop")`
+    padding: 0 3rem;
     height: 28rem;
+    gap: 0rem;
   `}
 
   position: relative;
@@ -31,6 +43,7 @@ export const ContentWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     gap: 0.5rem;
+    text-align: center;
 
     font-family: "Poppins", serif;
 
@@ -40,13 +53,11 @@ export const ContentWrapper = styled.div`
     `}
 
     ${media.greaterThan("desktop")`
-      align-items: center;
-      gap: 0.8rem;
-      margin-left: 15rem;
+      width: 100%;
     `}
 
     > h2 {
-      font-size: clamp(1.2rem, 1rem + 1vw, 2.8rem);
+      font-size: clamp(1.3rem, 0.7rem + 1vw, 2rem);
       font-weight: 500;
 
       ${media.between("tablet", "desktop")`
@@ -54,11 +65,12 @@ export const ContentWrapper = styled.div`
       `}
 
       ${media.greaterThan("desktop")`
-        font-size: clamp(4rem, 3rem + 2vw, 5rem);
+        font-size: clamp(3rem, 3rem + 1vw, 8rem);
       `}
     }
     > p {
-      font-size: clamp(1rem, 0.4rem + 1vw, 1.6rem);
+      font-size: clamp(0.7rem, 0.3rem + 1vw, 1.4rem);
+      
     }
   }
 `;
