4f30e9b232ae5a925d1a514673ba478ef58842d5
viniciuspra <vinicius.cascaesp@gmail.com>
2023-08-02 17:26:37 -0300

    feat: Update Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
-import Header from "../../components/Header";
 import { Container } from "./styles";
 
+import Banner from "../../components/Banner";
+import Section from "../../components/Section";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 export default function Home() {
   return (
     <Container>
-      <Header />
+      <Header/>
+      <Banner/>
+      <Section title="Refeições" />
+      <Section title="Sobremesas" />
+      <Section title="Bebidas" />
+      <Footer />
     </Container>
   );
 }
