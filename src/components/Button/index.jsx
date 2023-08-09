ce7c3394f4b0dd0cdbe124c0a53e182878f506c9
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-09 20:28:45 -0300

    feat: Add 'isblack' prop to Button component for black styling

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import { Container } from './styles';
 
-export default function Button({ text, icon: Icon, value, loading = false, ...rest }) {
+export default function Button({ text, icon: Icon, $isblack, value, loading = false, ...rest }) {
   return (
-    <Container {...rest} disabled={loading}>
+    <Container {...rest} disabled={loading} $isblack={$isblack}>
       {Icon && <Icon />}
       {text}
       {value && <span>{value}</span>}
