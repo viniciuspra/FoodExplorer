887f9f4b34b75e8d80d3fcc45c298b30fbd3757c
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-11 21:42:42 -0300

    feat: Responsive Header rendering with isAdmin prop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,19 @@ import { useMediaQuery } from "react-responsive";
 import HeaderDesktop from "../../components/HeaderDesktop";
 import HeaderMobile from "../../components/HeaderMobile";
 
+import { useAuth } from "../../hooks/auth";
+
 export default function Header() {
+  const { user, signOut } = useAuth();
   const isMobile = useMediaQuery({ maxWidth: 1024 });
 
-   return (
+  return (
     <Container>
-      {isMobile ? <HeaderMobile /> : <HeaderDesktop />}
+      {isMobile ? (
+        <HeaderMobile isAdmin={user.isAdmin} signOut={signOut} />
+      ) : (
+        <HeaderDesktop isAdmin={user.isAdmin} signOut={signOut} />
+      )}
     </Container>
-   )
+  );
 }
