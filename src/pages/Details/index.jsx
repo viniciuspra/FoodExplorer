580074718ba81e4642cb860b50801e2455ad8119
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-15 20:19:39 -0300

    feat: Populate dishes dynamically in 'Details' page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,6 @@
 import BackButton from "../../components/BackButton";
 import { Container, ContentWrapper, ButtonWrapper } from "./styles";
 
-import img from "../../assets/img.png"
 import Button from "../../components/Button";
 
 import Counter from "../../components/Counter";
@@ -9,38 +8,80 @@ import Receipt from "../../components/Receipt";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
-export default function Details() {
-  const ingredients = [
-    'alface',
-    'cheese',
-    'orange',
-    'tomato',
-    'pao de nam',
-    'alface'
-  ]
+import { useAuth } from "../../hooks/auth";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
 
-  const cost = '10.90'
+export default function Details() {
+  const { user } = useAuth();
+  const { id } = useParams();
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  const navigateEditPage = () => {
+    navigate(`/edit/${id}`);
+  };
+
+  useEffect(() => {
+    async function fetchDish() {
+      try {
+        const response = await api.get(`/dishes/${id}`);
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar prato:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchDish();
+  }, [id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <Container>
       <Header />
-      <BackButton to="/"/>
-      <ContentWrapper>
-        <img src={img} alt="prato" />
-        <h3>Salada Ravanello</h3>
-        <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</p>
-
-        <ul>
-          {ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))}
-        </ul>
-      </ContentWrapper>
-      <ButtonWrapper>
-        <Counter />
-        <Button icon={Receipt} text="pedir" value={`R$${cost}`}/> 
-      </ButtonWrapper>
+      <BackButton to="/" />
+      {data && data.image_url && (
+        <>
+          <ContentWrapper>
+            <img
+              src={`${api.defaults.baseURL}/files/${data.image_url}`}
+              alt="prato"
+            />
+            <div>
+              <h3>{data.name}</h3>
+              <p>{data.description}</p>
+              <ul>
+                {data.ingredients &&
+                  data.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient.name}</li>
+                  ))}
+              </ul>
+              <ButtonWrapper>
+                {!user.isAdmin ? <Counter /> : ""}
+                {!user.isAdmin ? (
+                  <Button
+                    icon={Receipt}
+                    text="pedir"
+                    value={`R$${data.price}`}
+                  />
+                ) : (
+                  <Button text="Editar prato" onClick={navigateEditPage} />
+                )}
+              </ButtonWrapper>
+            </div>
+          </ContentWrapper>
+        </>
+      )}
       <Footer />
     </Container>
-  )
+  );
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,35 +1,84 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { media } from "../../configs/breakpoints";
 
 export const Container = styled.div`
   height: 100vh;
+
+  > a:nth-child(2) {
+    margin: 2.4rem 2.4rem 0;
+
+    ${media.greaterThan("mobile")`
+      margin: 2.8rem 4rem 0;
+    `}
+    ${media.greaterThan("desktop")`
+      margin: 2.8rem 10rem -5rem;
+    `}
+  }
 `;
 
 export const ContentWrapper = styled.div`
-  margin: 2.4rem 4rem;
-
+  height: 100%;
+  margin: 2.4rem;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 1rem;
 
   text-align: center;
   font-family: "Poppins", serif;
 
   color: ${({ theme }) => theme.COLORS.WHITE_300};
+
+  ${media.greaterThan("mobile")`
+    margin: 2.4rem 4rem;
+  `}
+  ${media.greaterThan("desktop")`
+    width: clamp(90rem, 1vw, 120rem);
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    > img {
+      width: clamp(35rem, 5vw, 40rem);
+      margin: auto 0;
+    }
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      align-items: flex-start;
+      gap: 2rem;
+      margin-left: 4rem;
+      text-align: left;
+
+      padding: 2rem;
+
+      > h3 {
+        font-size: 4rem;
+      }
+      > p {
+        font-size: 2rem;
+      }
+    }
+  `}
+
+
   > img {
     max-width: 35rem;
   }
 
-  > h3 {
+  h3 {
     font-size: 2.7rem;
     font-weight: 500;
   }
-  > p {
+  p {
     font-size: 1.4rem;
     font-weight: 400;
   }
 
-  > ul {
+  ul {
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
@@ -37,12 +86,12 @@ export const ContentWrapper = styled.div`
     margin-top: 1rem;
     list-style: none;
     font-size: 1.3rem;
-
+    font-weight: 500;
     > li {
       background-color: ${({ theme }) => theme.COLORS.DARK_1000};
       border-radius: 0.4rem;
       margin-top: 1rem;
-      padding: 0.3rem;
+      padding: 0.5rem;
     }
   }
 `;
@@ -51,7 +100,13 @@ export const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 3rem;
+  max-width: 30rem;
+  margin: auto;
+  margin-top: 2.4rem;
 
-  margin: 4rem 4rem 0;
-`
+  ${media.greaterThan("desktop")`
+    width: 100%;
+    margin: 2.4rem;
+  `}
+`;
