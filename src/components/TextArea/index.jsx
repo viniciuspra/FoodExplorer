582ca84b8eb562843cf1d5e0b8c8837dd71ef5a8
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-15 20:21:01 -0300

    refactor: Update value handling in TextArea component

--- src/components/TextArea/index.jsx
+++ src/components/TextArea/index.jsx
@@ -2,8 +2,8 @@ import { Container } from "./styles";
 
 export default function TextArea({ value, ...rest }) {
   return (
-    <Container {...rest} >
-      {value}
+    <Container value={value} {...rest}>
+      
     </Container>
   )
 }
