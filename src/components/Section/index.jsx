e546bbf69c1da79d95b6b2c69ec2c5e77bca6e1f
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-15 20:22:06 -0300

    feat: Add description to DishCard for desktop devices

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,18 +1,15 @@
 import { Container } from "./styles";
-
 import { useAuth } from "../../hooks/auth";
-
 import DishCard from "../DishCard";
-
+import { api } from "../../services/api";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-
 import GetSliderSettings from "../../configs/SliderSettings";
 
 export default function Section({ title, dishes }) {
   const { user } = useAuth();
-  const settings = GetSliderSettings(dishes)
+  const settings = GetSliderSettings(dishes);
   
   return (
     <Container>
@@ -22,9 +19,10 @@ export default function Section({ title, dishes }) {
           <DishCard
             key={dish.id}
             id={dish.id}
-            title={dish.title}
+            title={dish.name}
             price={dish.price}
-            img={dish.img}
+            description={dish.description}
+            image_url={`${api.defaults.baseURL}/files/${dish.image_url}`}
             isAdmin={user.isAdmin}
           />
         ))}
