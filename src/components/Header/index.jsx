2bfe8139ada587fd99f5bc068427ad849c69f7ea
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-15 20:26:01 -0300

    refactor: Create responsive unified Header componen

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,117 @@
-import { Container } from "./styles";
-import { useMediaQuery } from "react-responsive";
-import HeaderDesktop from "../../components/HeaderDesktop";
-import HeaderMobile from "../../components/HeaderMobile";
-
 import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
-export default function Header() {
+import {
+  Container,
+  MenuButton,
+  SearchWrapper,
+  LogoWrapper,
+  ButtonWrapper,
+  InputWrapper,
+} from "./styles";
+
+import { Menu, LogOut, PlusCircle } from "lucide-react";
+import Receipt from "../Receipt";
+
+import Button from "../Button";
+import SearchMenu from "../SearchMenu";
+import InputSearch from "../InputSearch";
+import OrderControl from "../OrderControl";
+import LogoFoodExplorer from "../LogoFoodExplorer";
+
+import useLocalStorage from "../../hooks/useLocalStorage";
+
+export default function Header({ search, setSearch }) {
   const { user, signOut } = useAuth();
+
   const isMobile = useMediaQuery({ maxWidth: 1024 });
 
+  const navigate = useNavigate();
+
+  const { value } = useLocalStorage("order-items");
+
+  const [searchMenuOpen, setsearchMenuOpen] = useState(false);
+
+  const onToggleSearchMenu = () => {
+    setsearchMenuOpen(!searchMenuOpen);
+  };
+
+  const handleSignOut = async () => {
+    await signOut();
+    navigate("/");
+  };
+
+  const NavigateNew = () => {
+    navigate("/new");
+  };
+
   return (
-    <Container>
+    <>
       {isMobile ? (
-        <HeaderMobile isAdmin={user.isAdmin} signOut={signOut} />
+        <>
+          {!searchMenuOpen ? (
+            <Container>
+              <MenuButton onClick={onToggleSearchMenu}>
+                <Menu stroke="white" size={28} />
+              </MenuButton>
+              <LogoWrapper>
+                <LogoFoodExplorer
+                  fill="#065E7C"
+                  stroke="#065E7C"
+                  size="24"
+                  isAdmin={user.isAdmin}
+                />
+              </LogoWrapper>
+              {!user.isAdmin ? <OrderControl /> : ""}
+            </Container>
+          ) : (
+            <SearchWrapper>
+              <SearchMenu
+                onClose={onToggleSearchMenu}
+                isAdmin={user.isAdmin}
+                signOut={handleSignOut}
+                setSearch={setSearch}
+                search={search}
+              />
+            </SearchWrapper>
+          )}
+        </>
       ) : (
-        <HeaderDesktop isAdmin={user.isAdmin} signOut={signOut} />
+        <Container>
+          <LogoFoodExplorer
+            fill="#065E7C"
+            stroke="#065E7C"
+            size="32"
+            isAdmin={user.isAdmin}
+          />
+          <InputWrapper>
+            <InputSearch
+              type="text"
+              placeholder="Busque por pratos ou ingredientes"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </InputWrapper>
+          <ButtonWrapper>
+            {!user.isAdmin ? (
+              <Button
+                icon={Receipt}
+                text="Pedidos"
+                value={`(${value.length})`}
+              />
+            ) : (
+              <Button
+                icon={PlusCircle}
+                text="Criar Prato"
+                onClick={NavigateNew}
+              />
+            )}
+            <LogOut size={40} cursor="pointer" onClick={handleSignOut} />
+          </ButtonWrapper>
+        </Container>
       )}
-    </Container>
+    </>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,73 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { media } from "../../configs/breakpoints";
 
-export const Container = styled.div`
-  
+export const Container = styled.header`
+  background-color: ${({ theme }) => theme.COLORS.DARK_700};
+  padding: 5.6rem 2.8rem 2.4rem;
+
+  ${media.greaterThan("mobile")`
+    padding: 5.6rem 4rem 2.4rem;
+  `}
+
+  ${media.greaterThan("desktop")`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+    padding: 3rem 10rem;
+  `}
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > svg {
+    cursor: pointer;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 3.2rem;
 `
+
+export const InputWrapper = styled.div`
+  flex: 1;
+  padding: 0 3.2rem;
+`
+
+export const LogoWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SearchWrapper = styled.div`
+  overflow: hidden;
+  height: 100;
+`;
+
+export const MenuButton = styled.button`
+  background: transparent;
+  border: none;
+`;
+
+export const CloseButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  background: none;
+  border: none;
+
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: 1.6rem;
+  font-family: "Poppins", serif;
+  font-family: 300;
+
+  cursor: pointer;
+  outline: none;
+`;
