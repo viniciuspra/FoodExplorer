a66d5fe5ff062c709a7c08be8fa352ad1db1d957
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-15 20:16:25 -0300

    feat: Display conditional sections and add search functionality on Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,62 @@
-import { Container } from "./styles";
-
+import { useEffect, useState } from "react";
+import { Container, NoDishesMessage } from "./styles";
 import Banner from "../../components/Banner";
 import Section from "../../components/Section";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { api } from "../../services/api";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [filteredSections, setFilteredSections] = useState([]);
+
+  useEffect(() => {
+    const fetchDishes = async () => {
+      try {
+        const response = await api.get(`/dishes?name=${search}`);
+        const fetchedDishes = response.data;
+
+        const filteredRefeicaoDishes = fetchedDishes.filter(
+          (dish) => dish.category === "refeicao"
+        );
+        const filteredSobremesaDishes = fetchedDishes.filter(
+          (dish) => dish.category === "sobremesa"
+        );
+        const filteredBebidasDishes = fetchedDishes.filter(
+          (dish) => dish.category === "bebida"
+        );
+
+        const sections = [
+          { title: "Refeições", dishes: filteredRefeicaoDishes },
+          { title: "Sobremesas", dishes: filteredSobremesaDishes },
+          { title: "Bebidas", dishes: filteredBebidasDishes },
+        ];
+        
+        setFilteredSections(sections);
+      } catch (error) {
+        console.error("Erro ao buscar pratos:", error);
+      }
+    };
+
+    fetchDishes();
+  }, [search]);
+
   return (
     <Container>
-      <Header/>
-      <Banner/>
-      <Section title="Refeições" />
-      <Section title="Sobremesas" />
-      <Section title="Bebidas" />
+      <Header search={search} setSearch={setSearch} />
+      <Banner />
+      {filteredSections.map((section) => (
+        section.dishes.length > 0 && (
+          <Section
+            key={section.title}
+            title={section.title}
+            dishes={section.dishes}
+          />
+        )
+      ))}
+      {filteredSections.every((section) => section.dishes.length === 0) && (
+        <NoDishesMessage>Nenhum prato cadastrado.</NoDishesMessage>
+      )}
       <Footer />
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,3 +4,12 @@ export const Container = styled.div`
   height: 100vh;
   overflow-x: hidden;
 `;
+
+export const NoDishesMessage = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-size: clamp(1.5rem, 4vw, 3rem);
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`;
