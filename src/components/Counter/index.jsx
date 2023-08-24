921ad323f7c7472d6406bac0d65bf0ec813a5c60
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-24 19:20:10 -0300

    feat: Update Counter to adjust price value based on counter changes

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Container, Button, Value } from "./styles";
 import { Minus, Plus } from "lucide-react";
 
-export default function Counter() {
+export default function Counter({ onCountChange }) {
   const [count, setCount] = useState(1)
 
   const handleDecrease = () => {
-    setCount((prevCount) => Math.max(0, prevCount - 1))
+    setCount((prevCount) => Math.max(1, prevCount - 1))
   }
 
   const handleIncrease  = () => {
     setCount((prevCount) => prevCount + 1)
   }
+
+  useEffect(() => {
+    onCountChange(count)
+  }, [count, onCountChange])
 
   const formattedCount = count < 10 ? `0${count}` : count;
 
