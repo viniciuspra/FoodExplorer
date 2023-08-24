27bb23b5ccfd9f3e4aea6eaa987ff2605df5c321
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-24 19:14:17 -0300

    feat: Add mobile check to DishCard and Add counter component

--- src/components/DishCard/index.jsx
+++ src/components/DishCard/index.jsx
@@ -1,31 +1,74 @@
 import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
 
-import { Container } from "./styles";
-import { ChevronRight, Heart } from "lucide-react";
+import { Container, ContentWrapper, LinkWrapper, Paragraph } from "./styles";
+import { ChevronRight, Heart, Pencil } from "lucide-react";
+import Counter from "../Counter";
+import Button from "../Button";
 
-import Counter from "../Counter"
-import Button from "../Button"
-
-export default function DishCard({ title, img, price }) {
+export default function DishCard({ id, title, image_url, price, description, isAdmin }) {
   const [isLiked, setIsLiked] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 1024 });
+
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  const formattedPrice = totalPrice.toFixed(2).replace(".", ",")
+  
+  const handleCountChange = (newCount) => {
+    setTotalPrice(newCount * price);
+  }
 
   const handleHeartClick = () => {
     setIsLiked((prevIsLiked) => !prevIsLiked);
   };
+
+  const truncateDescription = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + "...";
+    }
+    return text;
+  }
+
   return (
     <Container>
-      <Heart
-        size={24}
-        onClick={handleHeartClick}
-        fill={isLiked ? "white" : ""}
-        cursor="pointer"
-      />
-      <img src={img} alt="foto do prato" />
-      <h2><a href="">{title} <ChevronRight size={24}/></a></h2>
-      <span>R$ {price}</span>
-
-      <Counter />
-      <Button text="incluir"/>
+      {!isAdmin ? (
+        <Heart
+          size={24}
+          onClick={handleHeartClick}
+          fill={isLiked ? "white" : ""}
+          cursor="pointer"
+        />
+      ) : (
+        <LinkWrapper to={`/edit/${id}`}>
+          <Pencil size={24} />
+        </LinkWrapper>
+      )}
+      <ContentWrapper>
+        <Link to={`/details/${id}`}>
+          <img src={image_url} alt="foto do prato" />
+          <h2>
+            {title} <ChevronRight size={20} />
+          </h2>
+          {
+            !isMobile && 
+            <Paragraph>
+              <p>{truncateDescription(description, 50)}</p>
+            </Paragraph>
+          }
+          {
+            !isAdmin ? <span>R$ {formattedPrice}</span> : '' 
+          }
+        </Link>
+        {!isAdmin ? (
+          <>
+            <Counter onCountChange={handleCountChange}/>
+            <Button text="incluir" />
+          </>
+        ) : (
+          ""
+        )}
+      </ContentWrapper>
     </Container>
   );
 }
--- src/components/DishCard/styles.js
+++ src/components/DishCard/styles.js
@@ -1,44 +1,71 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   position: relative;
   width: clamp(20rem, 12rem + 10vw, 28rem);
   background-color: ${({ theme }) => theme.COLORS.DARK_200};
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: clamp(0.3rem, 0.2rem + 1vw, 1.2rem);
-
   border: 1px solid ${({ theme }) => theme.COLORS.DARK_300};
   border-radius: 0.6rem;
 
-  padding: 2.4rem;
-
-  > img {
-    width: clamp(8rem, 4rem + 10vw, 15rem);
-  }
-
-  > h2 > a {
-    display: flex;
-    align-items: center;
-    gap: 0.2rem;
-    font-size: clamp(1.5rem, 0.9rem + 3vw, 2.5rem);
-    text-decoration: none;
-    color: ${({ theme }) => theme.COLORS.WHITE};
-  }
-
-  > span {
-    display: flex;
-    align-items: center;
-    font-size: clamp(1.7rem, 0.6rem + 1vw, 2.5rem);
-    color: ${({ theme }) => theme.COLORS.CAKE_100};
-  }
+  padding: 3rem 2.4rem;
 
   > svg {
     position: absolute;
     top: 1.6rem;
     right: 1.6rem;
   }
+`;
 
+export const LinkWrapper = styled(Link)`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  position: absolute;
+  top: 1.6rem;
+  right: 1.6rem;
+`;
+
+export const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: clamp(0.3rem, 0.2rem + 1vw, 1.2rem);
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    > a {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+      border-radius: 20%;
+      
+    > img {
+      width: clamp(8rem, 4rem + 10vw, 15rem);
+    }
+    > h2 {
+      display: flex;
+      align-items: center;
+      gap: 0.2rem;
+      font-size: clamp(1.5rem, 0.5rem + 1vw, 2.5rem);
+      text-decoration: none;
+    }
+
+    > span {
+      display: flex;
+      align-items: center;
+      font-size: clamp(1.5rem, 0.4rem + 1vw, 2.5rem);
+      color: ${({ theme }) => theme.COLORS.CAKE_100};
+    }
+  }
+`;
+
+export const Paragraph = styled.div`
+  max-width: 25rem;
+  font-size: 1.2rem;
+  text-align: center;
+  overflow: hidden; 
+  text-overflow: ellipsis;
 `
