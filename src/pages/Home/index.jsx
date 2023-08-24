f21965980f2d36637674fbe6bab8210359ee4ceb
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-08-24 19:25:22 -0300

    feat: Conditionally render sections on Home based on available dishes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,25 +13,51 @@ export default function Home() {
   useEffect(() => {
     const fetchDishes = async () => {
       try {
-        const response = await api.get(`/dishes?name=${search}`);
-        const fetchedDishes = response.data;
+        const nameResponse = await api.get(`/dishes?name=${search}`);
+        const ingredientResponse = await api.get(
+          `/dishes?name&ingredients=${search}`
+        );
 
-        const filteredRefeicaoDishes = fetchedDishes.filter(
-          (dish) => dish.category === "refeicao"
-        );
-        const filteredSobremesaDishes = fetchedDishes.filter(
-          (dish) => dish.category === "sobremesa"
-        );
-        const filteredBebidasDishes = fetchedDishes.filter(
-          (dish) => dish.category === "bebida"
-        );
+        const fetchedNameDishes = nameResponse.data;
+        const fetchedIngredientsDishes = ingredientResponse.data;
+
+        const combinedDishes = [
+          ...fetchedNameDishes,
+          ...fetchedIngredientsDishes,
+        ];
+
+        let filteredRefeicaoDishes;
+        let filteredSobremesaDishes;
+        let filteredBebidasDishes;
+
+        if (fetchedIngredientsDishes.length === fetchedNameDishes.length) {
+          filteredRefeicaoDishes = fetchedNameDishes.filter(
+            (dish) => dish.category === "refeicao"
+          );
+          filteredSobremesaDishes = fetchedNameDishes.filter(
+            (dish) => dish.category === "sobremesa"
+          );
+          filteredBebidasDishes = fetchedNameDishes.filter(
+            (dish) => dish.category === "bebida"
+          );
+        } else {
+          filteredRefeicaoDishes = combinedDishes.filter(
+            (dish) => dish.category === "refeicao"
+          );
+          filteredSobremesaDishes = combinedDishes.filter(
+            (dish) => dish.category === "sobremesa"
+          );
+          filteredBebidasDishes = combinedDishes.filter(
+            (dish) => dish.category === "bebida"
+          );
+        }
 
         const sections = [
           { title: "Refeições", dishes: filteredRefeicaoDishes },
           { title: "Sobremesas", dishes: filteredSobremesaDishes },
           { title: "Bebidas", dishes: filteredBebidasDishes },
         ];
-        
+
         setFilteredSections(sections);
       } catch (error) {
         console.error("Erro ao buscar pratos:", error);
@@ -45,15 +71,16 @@ export default function Home() {
     <Container>
       <Header search={search} setSearch={setSearch} />
       <Banner />
-      {filteredSections.map((section) => (
-        section.dishes.length > 0 && (
-          <Section
-            key={section.title}
-            title={section.title}
-            dishes={section.dishes}
-          />
-        )
-      ))}
+      {filteredSections.map(
+        (section) =>
+          section.dishes.length > 0 && (
+            <Section
+              key={section.title}
+              title={section.title}
+              dishes={section.dishes}
+            />
+          )
+      )}
       {filteredSections.every((section) => section.dishes.length === 0) && (
         <NoDishesMessage>Nenhum prato cadastrado.</NoDishesMessage>
       )}
