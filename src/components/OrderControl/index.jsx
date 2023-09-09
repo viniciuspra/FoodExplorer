1788d2c4e382111a2fd77c733deb350b504eea21
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-09-09 20:18:19 -0300

    refactor: Update cart total calculation

--- src/components/OrderControl/index.jsx
+++ src/components/OrderControl/index.jsx
@@ -5,7 +5,7 @@ import { useCartContext } from "../../hooks/cart-items";
 export default function OrderControl() {
   const { cartItems } = useCartContext();
 
-  const totalQuantity = cartItems ? cartItems.reduce((total, item) => total + item.id.quantity, 0) : 0;
+  const totalQuantity = cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
 
   return (
     <Container to="/cart">
