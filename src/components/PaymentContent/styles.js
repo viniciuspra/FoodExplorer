b36568cad2049c4da9dd1f8f1df814df479c1399
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-09-09 20:18:33 -0300

    style: Update style

--- src/components/PaymentContent/styles.js
+++ src/components/PaymentContent/styles.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
+  max-width: 50rem;
+  height: 35rem;
   padding: 4rem 1rem;
 
   border: 1px solid #fff;
